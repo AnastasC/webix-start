f48027b4a32d21aad2e7cf945cbfb4d7aafa78dc
Anastasiya Cherniavskaya <103057157+AnastasC@users.noreply.github.com>
2022-04-12 12:27:01 +0300

    feat: add confirm action before deleting category

---
+++ adminsDatatable.js
@@ -0,0 +1,40 @@
+import { categories } from "../../../datas/collection.js";
+
+const adminsDatatable = {
+    view: "datatable",
+    id: "admins_datatable",
+    editable: true,
+    scrollX: false,
+    select: true,
+    columns: [
+        { 
+          id:"id",
+          header: "",
+          width: 50,
+        },
+        { 
+          id: "value",
+          header: "Category",
+          fillspace: true,
+          editor: "text",
+          fillspace: true,
+        },
+        { 
+          id: "delete",
+          header: "",
+          template: "{common.trashIcon()}",
+      },
+    ], 
+    onClick: {
+      "wxi-trash": (e, id) => {
+        webix.confirm({
+            title: "Delete...",
+            text: "Do you still want to delete this category",
+          }).then(
+              () => categories.remove(id),
+        );
+      },
+    },   
+};
+
+export default adminsDatatable;
